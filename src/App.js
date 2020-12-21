fcc4347df43752bba44b85fed5131df8b9afc87b
Daren McCarrellgit config --global user.email darenmcc16@gmail.com <darenmcc16@gmail.com>
2020-12-20 19:43:52 -0600

    accordion app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import React, {Component} from 'react';
 import './App.css';
-import Messages from './Messages';
-import HelloWorld from './state-drills/HelloWorld';
-import Counter from './state/Counter';
-import TheDate from './state/TheDate';
-import Bomb from './state-drills/Bomb';
-import RouletteGun from './state-drills/RouletteGun';
+// import Messages from './Messages';
+// import HelloWorld from './state-drills/HelloWorld';
+// import Counter from './state/Counter';
+// import TheDate from './state/TheDate';
+// import Bomb from './state-drills/Bomb';
+// import RouletteGun from './state-drills/RouletteGun';
+//import Tabs from './state/Tabs';
+import Accordion from './state-drills/Accordion';
+
+const sections = [
+  {
+    title: 'Section 1',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+  },
+  {
+    title: 'Section 2',
+    content: 'Cupiditate tenetur aliquam necessitatibus id distinctio quas nihil ipsam nisi modi!',
+  },
+  {
+    title: 'Section 3',
+    content: 'Animi amet cumque sint cupiditate officia ab voluptatibus libero optio et?',
+  },
+];
 
 class App extends Component{
   render(){
     return(
       <div className="App">
-        <h1>YOUR APPLICATION NAME!</h1>
-        <Messages name="Messages" unread={0} />
-        <Messages name="Notifications" unread={10} />
-        <TheDate  name="Date" />
-        <Counter name="Counter" count={123}/>
-        <HelloWorld name="Hello" />
-        <Bomb name="BombApp" />
-        <RouletteGun name="RouletteGun" />
+        <Accordion sections = {sections}/>
       </div>
     );
   }
